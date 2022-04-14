2298d3560b1dd11f70ca9bb14c651df148b3f8eb
Maria Cláudia <91067315+mariaclaudiapio@users.noreply.github.com>
2022-04-14 19:44:35 -0300

    my version

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,16 @@
 var engine = {
-    "cores": ['green', 'purple', 'pink', 'red', 'yellow', 'black', 'orange', 'grey', 'blue', 'white'],
+    "cores": ['vert', 'violet', 'rose', 'rouge', 'jaune', 'noir', 'orange', 'gris', 'bleu', 'blanc'],
     "hexadecimais": {
-        'green': '#02EF00',
+        'violet': '#02EF00',
         'purple': '#790093',
-        'pink': '#FF0066',
-        'red': '#E90808',
-        'yellow': '#E7D703',
-        'black': '#141414',
+        'rose': '#FF0066',
+        'rouge': '#E90808',
+        'jaune': '#E7D703',
+        'noir': '#141414',
         'orange': '#F16529',
-        'grey': '#999999',
-        'blue': '#0000FF',
-        'white':'#FFFFFF',
+        'gris': '#999999',
+        'bleu': '#0000FF',
+        'blanc':'#FFFFFF',
     },
     "moedas": 0
 }
@@ -49,3 +49,55 @@ function atualizaPontuacao(valor) {
 
     pontuacao.innerText = engine.moedas;
 }
+
+aplicarCorNaCaixa(sortearCor())
+
+//API de reconhecimento de voz:
+
+var btnGravador = document.getElementById("botao-responder");
+var transcricaoAudio = "";
+var respostaCorreta = "";
+
+if(window.SpeechRecognition || window.webkitSpeechRecognition){
+  var SpeechAPI = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var gravador = new SpeechAPI();
+
+  gravador.continuos = false;
+  gravador.lang = "fr-fr";
+
+
+  gravador.onstart = function(){
+    btnGravador.innerText = "Estou Ouvindo";
+    btnGravador.style.backgroundColor = "white";
+    btnGravador.style.color = "black";
+  }
+
+  gravador.onend = function(){
+    btnGravador.innerText = "Responder";
+    btnGravador.style.backgroundColor = "transparent";
+    btnGravador.style.color = "white";
+  }
+
+  gravador.onresult = function(event){
+    transcricaoAudio = event.results[0][0].transcript.toUpperCase();
+    respostaCorreta = document.getElementById('cor-na-caixa').innerText.toUpperCase();
+
+    if(transcricaoAudio ===  respostaCorreta){
+      atualizaPontuacao(1);
+    }else{
+      atualizaPontuacao(-1);
+    }
+
+    aplicarCorNaCaixa(sortearCor());
+
+  }
+
+
+}else{
+  alert('não tem suporte');
+}
+
+
+btnGravador.addEventListener('click', function(e){
+  gravador.start();
+})
